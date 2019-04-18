8c2997d716d22bfe1843eab0423455955c88d526
Josh Single <joshsingle53@gmail.com>
2019-04-18 15:07:16 +0100

    done

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc(temp_f)
+    temp_c = (temp_f.to_f - 32) * (5.0/9.0)
+    return temp_c 
+end
+
+def ctof(temp_c)
+    temp_f = (temp_c.to_f) * (9.0/5.0) + 32
+    return temp_f
+end

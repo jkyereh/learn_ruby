d5109174af2dc78a0145a536f1285833a52afca9
Josh Single <joshsingle53@gmail.com>
2019-04-23 07:16:29 +0000

    done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,25 @@
 #write your code here
+def add(x,y)
+  return x + y
+end
+def subtract(x,y)
+  return x-y
+end
+def sum(x)
+  total = 0
+  x.each{|i| total += i}
+  return total
+end
+def multiply(x,y)
+  return x*y
+end
+def power(x,y)
+  return x**y
+end
+def factorial(x)
+  total = 1
+  while(x >= 1) do
+    total *= x
+    x -= 1
+  end
+end
